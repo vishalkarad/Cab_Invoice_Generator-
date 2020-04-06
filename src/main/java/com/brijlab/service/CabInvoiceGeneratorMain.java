15781eeb0221b2864ed6b093aa6ee07cfccedef1
vishalkarad <vishalkarad777@gmail.com>
2020-04-06 15:25:16 +0530

    Welcome to Cab Invoice

---
+++ src/main/java/com/brijlab/service/CabInvoiceGeneratorMain.java
@@ -0,0 +1,8 @@
+package com.brijlab.service;
+public class CabInvoiceGeneratorMain {
+
+    public static void main(String[] args) {
+        System.out.println("*************** Welcome to Cab Invoice Generator ***************");
+    }
+
+}
---
+++ src/test/java/com/brijlab/test/CabInvoiceGenratorTest.java
@@ -0,0 +1,14 @@
+package com.brijlab.test;
+import com.brijlab.service.CabInvoiceGeneratorMain;
+import org.junit.Test;
+
+public class CabInvoiceGenratorTest {
+
+    // Main class object
+    CabInvoiceGeneratorMain cabInvoiceGenerator = new CabInvoiceGeneratorMain();
+    @Test
+    public void welcomeMesage() {
+        CabInvoiceGeneratorMain cabInvoiceGeneratorMain = new CabInvoiceGeneratorMain();
+    }
+
+}
