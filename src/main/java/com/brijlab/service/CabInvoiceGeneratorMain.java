4f714fd49e8a79f081498f1ed7c9603a624a5468
vishalkarad <vishalkarad777@gmail.com>
2020-04-06 21:55:25 +0530

    Added : Add test case to total fare of journey

--- src/main/java/com/brijlab/service/CabInvoiceGeneratorMain.java
+++ src/main/java/com/brijlab/service/CabInvoiceGeneratorMain.java
@@ -1,8 +1,17 @@
 package com.brijlab.service;
 public class CabInvoiceGeneratorMain {
 
+    private static final double COST_PER_KILOMETER = 10;
+    private static final int COST_PER_MINUTE = 1;
+    private static final int MINIMUM_FARE = 5;
     public static void main(String[] args) {
         System.out.println("*************** Welcome to Cab Invoice Generator ***************");
     }
 
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * COST_PER_KILOMETER + time * COST_PER_MINUTE;
+        if (totalFare <= MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return totalFare;
+    }
 }
