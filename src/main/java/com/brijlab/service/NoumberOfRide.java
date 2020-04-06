6347d5235d99ed9928008177c1d47eac243b17c2
vishalkarad <vishalkarad777@gmail.com>
2020-04-06 22:57:34 +0530

    Added : Add class of Noumber of rides

--- src/main/java/com/brijlab/service/NoumberOfRide.java
+++ src/main/java/com/brijlab/service/NoumberOfRide.java
@@ -1,4 +1,10 @@
-package com.brijlab.test;
+package com.brijlab.service;
 
 public class NoumberOfRide {
+    public double distance;
+    public int time;
+    public NoumberOfRide(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
 }
