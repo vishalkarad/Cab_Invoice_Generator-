c92d83b9970e041d95aa23dfa74473378ec52b73
vishalkarad <vishalkarad777@gmail.com>
2020-04-07 00:40:56 +0530

    Added : Add new class Invoice Summary

--- src/main/java/com/brijlab/service/InvoiceSummary.java
+++ src/main/java/com/brijlab/service/InvoiceSummary.java
@@ -1,4 +1,24 @@
 package com.brijlab.service;
 
 public class InvoiceSummary {
+
+    public int numberOfRide;
+    public double totalFare;
+    public double averageFare;
+
+    public InvoiceSummary(int numberOfRides,double totalFare) {
+        this.numberOfRide = numberOfRides;
+        this.totalFare = totalFare;
+        this.averageFare = totalFare;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InvoiceSummary that = (InvoiceSummary) o;
+        return numberOfRide == that.numberOfRide &&
+                Double.compare(that.totalFare, totalFare) == 0 &&
+                Double.compare(that.averageFare, averageFare) == 0;
+    }
 }
