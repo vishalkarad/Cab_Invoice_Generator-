595c9a3566719710a3dad58b70c4ed25acaf293b
vishalkarad <vishalkarad777@gmail.com>
2020-04-06 21:56:26 +0530

    Added : Add function to genrate total fare of journey

--- src/test/java/com/brijlab/test/CabInvoiceGenratorTest.java
+++ src/test/java/com/brijlab/test/CabInvoiceGenratorTest.java
@@ -1,5 +1,6 @@
 package com.brijlab.test;
 import com.brijlab.service.CabInvoiceGeneratorMain;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CabInvoiceGenratorTest {
@@ -11,4 +12,19 @@ public class CabInvoiceGenratorTest {
         CabInvoiceGeneratorMain cabInvoiceGeneratorMain = new CabInvoiceGeneratorMain();
     }
 
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFareOfJourney() {
+        int time = 10;
+        double distance = 5.0;
+        double fare = cabInvoiceGenerator.calculateFare(distance,time);
+        Assert.assertEquals(60,fare,0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTime_WhenMnimumFare_ReturnTrue() {
+        int time = 0;
+        double distance = 0.1;
+        double fare = cabInvoiceGenerator.calculateFare(distance,time);
+        Assert.assertEquals(5,fare,0.0);
+    }
 }
